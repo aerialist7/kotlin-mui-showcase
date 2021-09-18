dbc6ced1eba8c5d47f1e8c5c0892efd069fbac35
aerialist7 <aerialist7@github.com>
2021-09-18 22:27:50 +0400

    Add `Ring UI`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("js") version "1.5.30"
+    id("com.github.turansky.kfc.webpack") version "4.32.0"
 }
 
 repositories {
@@ -11,8 +12,12 @@ fun kotlinw(target: String): String =
 
 dependencies {
     implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.246-kotlin-1.5.30")))
+
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
+
+    implementation(kotlinw("ring-ui"))
+    implementation(npm("core-js", "3.17.3"))
 }
 
 kotlin.js {
@@ -23,4 +28,16 @@ kotlin.js {
 tasks.wrapper {
     gradleVersion = "7.2"
     distributionType = Wrapper.DistributionType.ALL
+}
+
+tasks {
+    patchWebpackConfig {
+        patch(
+            """
+            config.module.rules.push(
+                ...require('@jetbrains/ring-ui/webpack.config').config.module.rules
+            )
+        """
+        )
+    }
 }
