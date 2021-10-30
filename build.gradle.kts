0f7f9550074e16f412874de4c1a61e356a07c826
aerialist7 <aerialist7@github.com>
2021-10-30 11:33:17 +0400

    Migrate on Kotlin Wrappers v.0.0.1-pre.262-kotlin-1.5.31

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.261-kotlin-1.5.31")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.262-kotlin-1.5.31")))
 
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
