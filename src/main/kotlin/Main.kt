94dd942b6819a14cf60a02e95366b82696bf4616
aerialist7 <aerialist7@github.com>
2021-10-30 18:58:17 +0400

    `ComponentPanel` -> `Showcase`

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
-import component.ComponentPanel
 import component.Footer
 import component.Header
+import component.Showcase
 import kotlinx.browser.document
 import react.dom.render
 
 fun main() {
     render(document.getElementById("root")) {
         Header()
-        ComponentPanel()
+        Showcase()
         Footer()
     }
 }
---
+++ src/main/kotlin/component/Showcase.kt
@@ -0,0 +1,66 @@
+package component
+
+import react.FunctionComponent
+import react.Props
+import react.fc
+import react.useState
+import ringui.SmartTabs
+import ringui.Tab
+import ringui.TabsProps
+import showcase.*
+
+external interface ComponentPanelProps : Props
+
+val Showcase = fc<ComponentPanelProps> {
+    var activeShowcase by useState("Accordion")
+
+    SmartTabs {
+        // TODO: Fix SmartTabs props
+        attrs.unsafeCast<TabsProps>().apply {
+            selected = activeShowcase
+            onSelect = { activeShowcase = it }
+        }
+        showcases.map { (showcaseName) ->
+            Tab {
+                attrs {
+                    id = showcaseName
+                    title = showcaseName
+                }
+            }
+        }
+    }
+    // TODO: Use react-router instead
+    showcases.getValue(activeShowcase)()
+}
+
+private val showcases: Map<String, FunctionComponent<Props>>
+    get() = mapOf(
+        "Accordion" to AccordionShowcase,
+        "Alert" to AlertShowcase,
+        "App Bar" to AppBarShowcase,
+        "Autocomplete" to AutocompleteShowcase,
+        "Avatar" to AvatarShowcase,
+        "Backdrop" to BackdropShowcase,
+        "Badge" to BadgeShowcase,
+        "Bottom Navigation" to BottomNavigationShowcase,
+        "Button" to ButtonShowcase,
+        "Card" to CardShowcase,
+        "Checkbox" to CheckboxShowcase,
+        "Chip" to ChipShowcase,
+        "Dialog" to DialogShowcase,
+        "Drawer" to DrawerShowcase,
+        "Fab" to FabShowcase,
+        "Image List" to ImageListShowcase,
+        "Input" to InputShowcase,
+        "Linear Progress" to LinearProgressShowcase,
+        "Link" to LinkShowcase,
+        "List" to ListShowcase,
+        "Menu" to MenuShowcase,
+        "Mobile Stepper" to MobileStepperShowcase,
+        "Pagination" to PaginationShowcase,
+        "Paper" to PaperShowcase,
+        "Popover" to PopoverShowcase,
+        "Popper" to PopperShowcase,
+        "Radio" to RadioShowcase,
+        "Rating" to RatingShowcase,
+    )
