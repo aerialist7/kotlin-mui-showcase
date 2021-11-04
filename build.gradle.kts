0544850299dae487e2caf665494e7f2572ccd89d
aerialist7 <aerialist7@github.com>
2021-11-04 14:18:19 +0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("js") version "1.5.31"
-    id("com.github.turansky.kfc.webpack") version "4.32.0"
+    id("com.github.turansky.kfc.webpack") version "4.42.0"
 }
 
 repositories {
@@ -11,7 +11,7 @@ fun kotlinw(target: String): String =
     "org.jetbrains.kotlin-wrappers:kotlin-$target"
 
 dependencies {
-    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.262-kotlin-1.5.31")))
+    implementation(enforcedPlatform(kotlinw("wrappers-bom:0.0.1-pre.264-kotlin-1.5.31")))
 
     implementation(kotlinw("react"))
     implementation(kotlinw("react-dom"))
@@ -19,22 +19,16 @@ dependencies {
 
     implementation(kotlinw("mui"))
 
-    implementation(kotlinw("ring-ui"))
-    implementation(npm("core-js", "3.19.0"))
-
     implementation(npm("@emotion/react", "11.5.0"))
     implementation(npm("@emotion/styled", "11.3.0"))
-    implementation(npm("@mui/material", "5.0.5"))
+
+    implementation(kotlinw("ring-ui"))
+    implementation(npm("core-js", "3.19.0"))
 }
 
 kotlin.js {
     browser()
     binaries.executable()
-}
-
-// TODO: Temporary until Kotlin update
-rootProject.plugins.withType(org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin::class.java) {
-    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().versions.webpackCli.version = "4.9.0"
 }
 
 tasks {
