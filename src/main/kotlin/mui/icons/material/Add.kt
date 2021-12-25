cc99d81a30f2dbc9f4461b785a2cc4662c73657d
aerialist7 <aerialist7@github.com>
2021-12-25 19:16:33 +0400

    Fab: Use `Add` icon

---
+++ src/main/kotlin/mui/icons/material/Add.kt
@@ -0,0 +1,7 @@
+@file:JsModule("@mui/icons-material/Add")
+@file:JsNonModule
+
+package mui.icons.material
+
+@JsName("default")
+external val Add: SvgIconComponent
--- src/main/kotlin/showcase/Fab.kt
+++ src/main/kotlin/showcase/Fab.kt
@@ -1,14 +1,14 @@
 package showcase
 
+import mui.icons.material.Add
 import mui.material.Fab
-import mui.material.SpeedDialIcon
 import react.FC
 import react.Props
 
 val FabShowcase = FC<Props> {
     Fab {
         color = "primary"
-        // TODO: No ability to use `AddIcon` from [MUI]
-        SpeedDialIcon()
+
+        Add()
     }
 }
