3e9860780091fc98b928b5541280357fe1d5ed0e
aerialist7 <aerialist7@github.com>
2021-12-25 19:43:14 +0400

    Slider: Use `VolumeUp` & `VolumeDown` icons

---
+++ src/main/kotlin/mui/icons/material/VolumeDown.kt
@@ -0,0 +1,7 @@
+@file:JsModule("@mui/icons-material/VolumeDown")
+@file:JsNonModule
+
+package mui.icons.material
+
+@JsName("default")
+external val VolumeDown: SvgIconComponent
--- src/main/kotlin/showcase/Slider.kt
+++ src/main/kotlin/showcase/Slider.kt
@@ -3,6 +3,8 @@ package showcase
 import csstype.AlignItems
 import csstype.px
 import kotlinext.js.jso
+import mui.icons.material.VolumeDown
+import mui.icons.material.VolumeUp
 import mui.material.Slider
 import mui.material.Stack
 import mui.system.ResponsiveStyleValue
@@ -17,15 +19,17 @@ val SliderShowcase = FC<Props> {
     var value by useState(30)
 
     Stack {
-        // TODO: Unable to use `spacing` prop value correctly [MUI]
-        spacing = 2.unsafeCast<ResponsiveStyleValue<dynamic>>()
-        // TODO: Unable to use `direction` prop value correctly [MUI]
-        direction = "row".unsafeCast<ResponsiveStyleValue<Union>>()
         sx = jso {
             marginBottom = 1.px
             alignItems = AlignItems.center
         }
-        // TODO: Unable to use `VolumeDown` component
+        // TODO: Unable to use `spacing` prop value correctly [MUI]
+        spacing = 2.unsafeCast<ResponsiveStyleValue<dynamic>>()
+        // TODO: Unable to use `direction` prop value correctly [MUI]
+        direction = "row".unsafeCast<ResponsiveStyleValue<Union>>()
+
+        VolumeDown()
+
         Slider {
             ariaLabel = "Volume"
             // TODO: Unable to use `value` prop correctly [MUI]
@@ -33,14 +37,15 @@ val SliderShowcase = FC<Props> {
             // TODO: Unable to use newValue as second parameter [MUI]
             onChange = { event -> value = 0 }
         }
-        // TODO: Unable to use `VolumeUp` component
+
+        VolumeUp()
     }
 
     Slider {
+        ariaLabel = "Disabled slider"
         // TODO: Unable to use `disabled` prop correctly [MUI]
         asDynamic().disabled = true
         // TODO: Need to use number `defaultValue` instead of string [MUI]
-        defaultValue = "30"
-        ariaLabel = "Disabled slider"
+        defaultValue = 30.unsafeCast<String>()
     }
 }
