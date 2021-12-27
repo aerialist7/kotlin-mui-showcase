653aaa15c7feffbdec9c71f78f1ccee265a5ccb0
aerialist7 <aerialist7@github.com>
2021-12-27 10:18:13 +0400

    Header: Add tooltips

--- src/main/kotlin/Header.kt
+++ src/main/kotlin/Header.kt
@@ -7,12 +7,10 @@ import kotlinext.js.jso
 import kotlinx.browser.window
 import mui.icons.material.GitHub
 import mui.icons.material.MenuBook
-import mui.material.AppBar
-import mui.material.IconButton
-import mui.material.Toolbar
-import mui.material.Typography
+import mui.material.*
 import react.FC
 import react.Props
+import react.ReactNode
 import react.dom.aria.AriaHasPopup.`false`
 import react.dom.aria.ariaHasPopup
 import react.dom.aria.ariaLabel
@@ -24,12 +22,12 @@ external interface HeaderProps : Props {
 
 val Header = FC<HeaderProps> { props ->
     AppBar {
-        position = "fixed"
         sx = jso {
             width = 100.pct - Sidebar.Width
             height = Header.Height
             marginLeft = Sidebar.Width
         }
+        position = "fixed"
 
         Toolbar {
             Typography {
@@ -43,38 +41,42 @@ val Header = FC<HeaderProps> { props ->
                 +"Kotlin MUI Showcase"
             }
 
-            IconButton {
-                size = "large"
-                color = "inherit"
-                title = "Read Documentation"
-                onClick = {
-                    window.location.href = "https://mui.com/components/${props.sourceCodeKey}/"
+            Tooltip {
+                title = ReactNode("Read Documentation")
+
+                IconButton {
+                    ariaLabel = "official documentation"
+                    ariaHasPopup = `false`
+                    size = "large"
+                    color = "inherit"
+                    onClick = {
+                        window.location.href = "https://mui.com/components/${props.sourceCodeKey}/"
+                    }
+
+                    MenuBook()
                 }
-
-                ariaLabel = "official documentation"
-                ariaHasPopup = `false`
-
-                MenuBook()
             }
 
-            IconButton {
-                size = "large"
-                color = "inherit"
-                title = "View Sources"
-                onClick = {
-                    val name = props.sourceCodeKey
-                        .split("-")
-                        .asSequence()
-                        .map { it.replaceFirstChar { it.titlecase() } }
-                        .reduce { accumulator, word -> accumulator.plus(word) }
+            Tooltip {
+                title = ReactNode("View Sources")
 
-                    window.location.href = "https://github.com/karakum-team/kotlin-mui-showcase/blob/main/src/main/kotlin/showcase/$name.kt"
+                IconButton {
+                    ariaLabel = "source code"
+                    ariaHasPopup = `false`
+                    size = "large"
+                    color = "inherit"
+                    onClick = {
+                        val name = props.sourceCodeKey
+                            .split("-")
+                            .asSequence()
+                            .map { it.replaceFirstChar { it.titlecase() } }
+                            .reduce { accumulator, word -> accumulator.plus(word) }
+
+                        window.location.href = "https://github.com/karakum-team/kotlin-mui-showcase/blob/main/src/main/kotlin/showcase/$name.kt"
+                    }
+
+                    GitHub()
                 }
-
-                ariaLabel = "source code"
-                ariaHasPopup = `false`
-
-                GitHub()
             }
         }
     }
