bba1d6742d13d8f592d81d86aaac98005d47e338
aerialist7 <aerialist7@github.com>
2022-01-02 22:00:57 +0400

    Cleanup

--- src/main/kotlin/example/App.kt
+++ src/main/kotlin/example/App.kt
@@ -1,11 +1,9 @@
 package example
 
 import csstype.Display
-import csstype.px
 import kotlinext.js.jso
 import kotlinx.browser.document
 import mui.system.Box
-import org.w3c.dom.HTMLDivElement
 import react.FC
 import react.Props
 import react.create
@@ -14,36 +12,21 @@ import react.router.dom.HashRouter
 
 fun main() {
     render(
-        element = HashRouter.create {
-            App()
-        },
-        container = document.createElement("div").unsafeCast<HTMLDivElement>().apply {
-            style.minHeight = "100vh"
-            style.display = "flex"
-            style.flexDirection = "column"
-            document.body!!.appendChild(this)
-        },
+        element = App.create(),
+        container = document.createElement("div").also { document.body!!.appendChild(it) },
     )
 }
 
 private val App = FC<Props> {
-    ShowcasesModule {
-        Box {
-            sx = jso { display = Display.flex }
+    HashRouter {
+        ShowcasesModule {
+            Box {
+                sx = jso { display = Display.flex }
 
-            Header()
-            Sidebar()
-            Content()
+                Header()
+                Sidebar()
+                Content()
+            }
         }
-    }
-}
-
-object Sizes {
-    object Header {
-        val Height = 64.px
-    }
-
-    object Sidebar {
-        val Width = 168.px
     }
 }
---
+++ src/main/kotlin/example/Sizes.kt
@@ -0,0 +1,14 @@
+package example
+
+import csstype.px
+
+// TODO: Remove
+object Sizes {
+    object Header {
+        val Height = 64.px
+    }
+
+    object Sidebar {
+        val Width = 168.px
+    }
+}
