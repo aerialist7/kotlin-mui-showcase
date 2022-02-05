4401b8a6528f75d33639c750e6bf71d007d6d0e1
vie10 <hrekhviktor@gmail.com>
2022-02-05 16:00:22 +0200

    Add switch with icon showcase

--- src/main/kotlin/example/component/Switches.kt
+++ src/main/kotlin/example/component/Switches.kt
@@ -1,8 +1,11 @@
 package example.component
 
+import mui.icons.material.AirplanemodeActive
+import mui.icons.material.AirplanemodeInactive
 import mui.material.Switch
 import react.FC
 import react.Props
+import react.create
 import react.dom.html.ReactHTML.div
 
 val SwitchesShowcase = FC<Props> {
@@ -18,6 +21,10 @@ val SwitchesShowcase = FC<Props> {
         }
         Switch {
             disabled = true
+        }
+        Switch {
+            icon = AirplanemodeInactive.create()
+            checkedIcon = AirplanemodeActive.create()
         }
     }
 }
