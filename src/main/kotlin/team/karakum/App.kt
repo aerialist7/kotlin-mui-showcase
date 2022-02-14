27a23177ffc628126b13c93e9db1a1d2c458172c
vie10 <hrekhviktor@gmail.com>
2022-02-14 16:14:11 +0200

    Adaptive stylization

--- src/main/kotlin/team/karakum/App.kt
+++ src/main/kotlin/team/karakum/App.kt
@@ -9,7 +9,9 @@ import react.Props
 import react.create
 import react.dom.render
 import react.router.dom.HashRouter
+import react.useContext
 import team.karakum.common.GridAreas
+import team.karakum.common.Sizes
 import team.karakum.common.Sizes.Header
 import team.karakum.common.Sizes.Sidebar
 import team.karakum.component.*
@@ -23,29 +25,51 @@ fun main() {
 
 private val App = FC<Props> {
     HashRouter {
-        ShowcasesModule {
-            ThemeModule {
-                Box {
-                    sx = jso {
-                        display = Display.grid
-                        gridTemplateRows = GridTemplateRows(
-                            Header.Height,
-                            Length.auto,
-                        )
-                        gridTemplateColumns = GridTemplateColumns(
-                            Sidebar.Width, Length.auto,
-                        )
-                        gridTemplateAreas = GridTemplateAreas(
-                            GridArea("${GridAreas.Header} ${GridAreas.Header}"),
-                            GridArea("${GridAreas.Sidebar} ${GridAreas.Content}"),
-                        )
+        AdaptiveModule {
+            ShowcasesModule {
+                SidebarModule {
+                    ThemeModule {
+                        AdaptiveAppView()
                     }
-
-                    Header()
-                    Sidebar()
-                    Content()
                 }
             }
         }
     }
+}
+
+private val AdaptiveAppView = FC<Props> {
+    val width by useContext(AdaptiveContext)
+    val isMobileViewRequired = width <= Sizes.Mobile.MaxWidth
+
+    Box {
+        sx = jso {
+            display = Display.grid
+            gridTemplateRows = GridTemplateRows(
+                Header.Height,
+                Length.auto,
+            )
+            gridTemplateColumns = GridTemplateColumns(
+                Sidebar.Width, Length.auto,
+            )
+            gridTemplateAreas = if (isMobileViewRequired) mobileTemplateAreas else defaultTemplateAreas
+        }
+
+        Header()
+        AdaptiveSidebar()
+        Content()
+    }
+}
+
+private val mobileTemplateAreas: GridTemplateAreas by lazy(LazyThreadSafetyMode.NONE) {
+    GridTemplateAreas(
+        GridArea("${GridAreas.Header} ${GridAreas.Header}"),
+        GridArea("${GridAreas.Content} ${GridAreas.Content}")
+    )
+}
+
+private val defaultTemplateAreas: GridTemplateAreas by lazy(LazyThreadSafetyMode.NONE) {
+    GridTemplateAreas(
+        GridArea("${GridAreas.Header} ${GridAreas.Header}"),
+        GridArea("${GridAreas.Sidebar} ${GridAreas.Content}")
+    )
 }
--- src/main/kotlin/team/karakum/component/Sidebar.kt
+++ src/main/kotlin/team/karakum/component/Sidebar.kt
@@ -1,26 +1,77 @@
 package team.karakum.component
 
-import csstype.Color
-import csstype.GridArea
-import csstype.TextDecoration
+import csstype.*
 import kotlinx.js.jso
 import mui.material.*
 import mui.system.Box
-import react.FC
-import react.Props
-import react.ReactNode
+import react.*
 import react.css.css
 import react.dom.html.ReactHTML
 import react.router.dom.NavLink
 import react.router.useLocation
-import react.useContext
 import team.karakum.common.GridAreas
 import team.karakum.common.Sizes
+import team.karakum.common.Sizes.Mobile
+import mui.icons.material.Menu as MenuIcon
+
+typealias SidebarState = StateInstance<Boolean>
+
+val SidebarContext = createContext<SidebarState>()
+
+val AdaptiveSidebar = FC<PropsWithChildren> { props ->
+    val width by useContext(AdaptiveContext)
+    val isMobileComponentRequired = width <= Mobile.MaxWidth
+
+    if (isMobileComponentRequired) {
+        MobileSidebar {
+            props.children()
+        }
+    } else {
+        Sidebar {
+            props.children()
+        }
+    }
+}
+
+val SidebarModule = FC<PropsWithChildren> { props ->
+    val state = useState(false)
+    SidebarContext.Provider(state) {
+        props.children()
+    }
+}
+
+val MobileSidebar = FC<Props> {
+    var opened by useContext(SidebarContext)
+
+    Box {
+        component = ReactHTML.nav
+
+        SwipeableDrawer {
+            open = opened
+            anchor = DrawerAnchor.left
+            onClose = {
+                opened = false
+            }
+
+            SidebarContent()
+        }
+    }
+    SpeedDial {
+        ariaLabel = "sidebar button"
+        sx = jso {
+            hidden = opened
+            position = Position.absolute
+            bottom = 16.px
+            left = 16.px
+        }
+        icon = MenuIcon.create()
+        onClick = {
+            opened = true
+        }
+    }
+}
 
 val Sidebar = FC<Props> {
-    val showcases = useContext(ShowcasesContext)
-    val lastPathname = useLocation().pathname.substringAfterLast("/")
-
     Box {
         component = ReactHTML.nav
         sx = jso {
@@ -31,27 +82,34 @@ val Sidebar = FC<Props> {
             variant = DrawerVariant.permanent
             anchor = DrawerAnchor.left
 
-            Toolbar()
+            SidebarContent()
+        }
+    }
+}
 
-            List {
-                sx = jso { width = Sizes.Sidebar.Width }
+val SidebarContent = FC<Props> {
+    val showcases = useContext(ShowcasesContext)
+    val lastPathname = useLocation().pathname.substringAfterLast("/")
 
-                for ((key, name) in showcases) {
-                    NavLink {
-                        to = key
+    Toolbar()
 
-                        css {
-                            textDecoration = TextDecoration.none
-                            color = Color.currentcolor
-                        }
+    List {
+        sx = jso { width = Sizes.Sidebar.Width }
 
-                        ListItemButton {
-                            selected = lastPathname == key
+        for ((key, name) in showcases) {
+            NavLink {
+                to = key
 
-                            ListItemText {
-                                primary = ReactNode(name)
-                            }
-                        }
+                css {
+                    textDecoration = TextDecoration.none
+                    color = Color.currentcolor
+                }
+
+                ListItemButton {
+                    selected = lastPathname == key
+
+                    ListItemText {
+                        primary = ReactNode(name)
                     }
                 }
             }
