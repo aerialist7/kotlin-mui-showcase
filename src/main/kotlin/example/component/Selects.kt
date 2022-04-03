fe1d30ea582d7c8b50f326d9de4710886ed86374
Peter Klimai <pklimai@gmail.com>
2022-04-03 22:32:22 +0300

    Fixes Select error

--- src/main/kotlin/example/component/Selects.kt
+++ src/main/kotlin/example/component/Selects.kt
@@ -27,9 +27,8 @@ val SelectsShowcase = FC<Props> {
                 // TODO: Needs an ability to set generic type to `Select` component [MUI]
                 value = age.unsafeCast<Nothing?>()
                 label = ReactNode("Age")
-                onChange = {
-                    // TODO: It doesn't work even with asDynamic. See console.log [MUI]
-                    age = it.asDynamic().target.value
+                onChange = { it: dynamic ->
+                    age = it.target.value
                 }
                 MenuItem {
                     value = "10"
