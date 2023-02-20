570ee3606ffc1db2fb4dd89adb071cd3a3aa0bcf
Alexander Popoff <alexander.popoff@flysto.net>
2023-02-20 22:04:24 +0200

    Wrappers `pre.500`

--- src/main/kotlin/team/karakum/App.kt
+++ src/main/kotlin/team/karakum/App.kt
@@ -1,16 +1,13 @@
 package team.karakum
 
-import js.core.ReadonlyArray
 import js.core.jso
 import react.FC
 import react.Props
-import react.ReactNode
 import react.create
 import react.dom.client.createRoot
-import react.router.RouteObject
+import react.router.NonIndexRouteObject
 import react.router.RouterProvider
 import react.router.dom.createHashRouter
-import remix.run.router.LoaderFunction
 import team.karakum.components.*
 import web.dom.document
 import web.html.HTML.div
@@ -25,13 +22,13 @@ fun main() {
 
 private val App = FC<Props> {
     val hashRouter = createHashRouter(
-        routes = arrayOf(
+        routes = arrayOf<NonIndexRouteObject>(
             jso {
                 path = "/"
                 loader = showcasesLoader
                 element = Showcases.create()
                 errorElement = Error.create()
-                children = arrayOf(
+                children = arrayOf<NonIndexRouteObject>(
                     jso {
                         path = ":showcaseId"
                         loader = showcaseLoader
@@ -53,34 +50,3 @@ private val App = FC<Props> {
         }
     }
 }
-
-// TODO: Remove when declarations will be updated
-private var RouteObject.loader: LoaderFunction
-    get() = asDynamic().loader
-    set(value) {
-        asDynamic().loader = value
-    }
-
-private var RouteObject.element: ReactNode
-    get() = asDynamic().element
-    set(value) {
-        asDynamic().element = value
-    }
-
-private var RouteObject.errorElement: ReactNode
-    get() = asDynamic().errorElement
-    set(value) {
-        asDynamic().errorElement = value
-    }
-
-private var RouteObject.path: String
-    get() = asDynamic().path
-    set(value) {
-        asDynamic().path = value
-    }
-
-private var RouteObject.children: ReadonlyArray<RouteObject>
-    get() = asDynamic().children
-    set(value) {
-        asDynamic().children = value
-    }
