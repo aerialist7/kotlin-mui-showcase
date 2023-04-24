1aef7b57047f1f1964f6dc1f8e2d13af064a3f29
Alexander Popoff <alexander.popoff@flysto.net>
2023-04-24 10:46:41 +0300

    Theming. Add `TypographyOptions` sample

--- src/main/kotlin/team/karakum/common/Themes.kt
+++ src/main/kotlin/team/karakum/common/Themes.kt
@@ -3,18 +3,38 @@ package team.karakum.common
 import js.core.jso
 import mui.material.PaletteMode.Companion.dark
 import mui.material.PaletteMode.Companion.light
+import mui.material.styles.TypographyOptions
+import mui.material.styles.TypographyVariant.Companion.h6
 import mui.material.styles.createTheme
+import web.cssom.integer
+import web.cssom.media.maxWidth
+import web.cssom.px
+import web.cssom.rem
+
+private val TYPOGRAPHY_OPTIONS = TypographyOptions {
+    fontWeight = integer(500)
+
+    h6 {
+        fontSize = 1.5.rem
+
+        media(maxWidth(599.px)) {
+            fontSize = 1.25.rem
+        }
+    }
+}
 
 object Themes {
     val Light = createTheme(
         jso {
             palette = jso { mode = light }
+            typography = TYPOGRAPHY_OPTIONS
         }
     )
 
     val Dark = createTheme(
         jso {
             palette = jso { mode = dark }
+            typography = TYPOGRAPHY_OPTIONS
         }
     )
 }
