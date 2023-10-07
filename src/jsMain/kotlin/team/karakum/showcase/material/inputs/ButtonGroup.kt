89ddecf7b9318337e48a0472672aeba024ef662d
Alexander Popoff <alexander.popoff@flysto.net>
2023-10-07 21:51:59 +0300

    Implement `ButtonGroup` showcase

--- src/jsMain/kotlin/team/karakum/showcase/material/inputs/ButtonGroup.kt
+++ src/jsMain/kotlin/team/karakum/showcase/material/inputs/ButtonGroup.kt
@@ -1,8 +1,27 @@
 package team.karakum.showcase.material.inputs
 
+import mui.material.Button
+import mui.material.ButtonGroup
+import mui.material.ButtonGroupVariant
 import react.FC
 import react.Props
+import react.dom.aria.ariaLabel
 
 val ButtonGroupShowcase = FC<Props> {
-    +"Not implemented"
+    ButtonGroup {
+        variant = ButtonGroupVariant.contained
+        ariaLabel = "outlined primary button group"
+
+        Button {
+            +"One"
+        }
+
+        Button {
+            +"Two"
+        }
+
+        Button {
+            +"Three"
+        }
+    }
 }
