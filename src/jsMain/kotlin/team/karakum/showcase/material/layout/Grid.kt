01fa68be97690b48a802d91fce0128d1c619685a
Alexander Popoff <alexander.popoff@flysto.net>
2023-10-07 22:17:26 +0300

    Implement `Grid` showcase

--- src/jsMain/kotlin/team/karakum/showcase/material/layout/Grid.kt
+++ src/jsMain/kotlin/team/karakum/showcase/material/layout/Grid.kt
@@ -1,8 +1,68 @@
 package team.karakum.showcase.material.layout
 
+import emotion.styled.styled
+import mui.material.Grid
+import mui.material.PaletteMode
+import mui.material.Paper
+import mui.material.styles.Theme
+import mui.system.responsive
 import react.FC
 import react.Props
+import team.karakum.xs
+import web.cssom.Color
+import web.cssom.TextAlign
 
 val GridShowcase = FC<Props> {
-    +"Not implemented"
+    Grid {
+        container = true
+        spacing = responsive(2)
+
+        Grid {
+            item = true
+            xs = 8
+
+            Item {
+                +"xs 8"
+            }
+        }
+
+        Grid {
+            item = true
+            xs = 4
+
+            Item {
+                +"xs 4"
+            }
+        }
+
+        Grid {
+            item = true
+            xs = 4
+
+            Item {
+                +"xs 4"
+            }
+        }
+
+
+        Grid {
+            item = true
+            xs = 8
+
+            Item {
+                +"xs 8"
+            }
+        }
+    }
+}
+
+// TODO: Cleanup after theme props improvements
+private val Item = Paper.styled { props ->
+    val theme: Theme = props.asDynamic().theme
+
+    backgroundColor = Color(if (theme.palette.mode == PaletteMode.dark) "#1A2027" else "#fff")
+    +theme.typography.body2
+    padding = theme.spacing(0.5.unsafeCast<Int>())
+    textAlign = TextAlign.center
+    color = theme.palette.text.secondary
 }
