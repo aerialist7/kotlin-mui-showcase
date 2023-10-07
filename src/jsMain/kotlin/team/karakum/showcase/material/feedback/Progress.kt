b479f5546e0c3525c0ae276365dde539db9c65c7
Alexander Popoff <alexander.popoff@flysto.net>
2023-10-07 21:44:21 +0300

    Move `Progress` showcase to correct place

--- src/jsMain/kotlin/team/karakum/showcase/material/feedback/Progress.kt
+++ src/jsMain/kotlin/team/karakum/showcase/material/feedback/Progress.kt
@@ -1,8 +1,25 @@
 package team.karakum.showcase.material.feedback
 
+import mui.material.LinearProgress
+import mui.material.LinearProgressColor
+import mui.material.Stack
+import mui.system.responsive
 import react.FC
 import react.Props
 
+// TODO: Add other progresses
 val ProgressShowcase = FC<Props> {
-    +"Not implemented"
+    Stack {
+        spacing = responsive(2)
+
+        LinearProgress {
+            color = LinearProgressColor.secondary
+        }
+        LinearProgress {
+            color = LinearProgressColor.success
+        }
+        LinearProgress {
+            color = LinearProgressColor.inherit
+        }
+    }
 }
