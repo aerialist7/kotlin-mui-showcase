b8688f56a569c262e70a9277441edace2baee3d1
porotkin <preload737773@gmail.com>
2024-03-03 14:23:51 +0400

    TextField Showcase: Use 8px gap instead of 1em

--- src/jsMain/kotlin/team/karakum/showcase/material/inputs/TextField.kt
+++ src/jsMain/kotlin/team/karakum/showcase/material/inputs/TextField.kt
@@ -17,7 +17,7 @@ import team.karakum.autoComplete
 import team.karakum.noValidate
 import web.cssom.Display
 import web.cssom.JustifyContent
-import web.cssom.em
+import web.cssom.px
 import web.html.InputType
 
 val TextFieldShowcase = FC<Props> {
@@ -25,7 +25,7 @@ val TextFieldShowcase = FC<Props> {
         sx {
             display = Display.grid
             justifyContent = JustifyContent.left
-            gap = 1.em
+            gap = 8.px
         }
         component = form
         noValidate = true
