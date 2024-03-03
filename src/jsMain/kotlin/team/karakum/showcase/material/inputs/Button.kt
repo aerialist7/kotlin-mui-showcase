5ef3a467b7599b34304fd8236fd5eb6ec7f8a188
porotkin <preload737773@gmail.com>
2024-03-03 13:34:35 +0400

    Add gap for `ButtonShowcase`

--- src/jsMain/kotlin/team/karakum/showcase/material/inputs/Button.kt
+++ src/jsMain/kotlin/team/karakum/showcase/material/inputs/Button.kt
@@ -1,23 +1,36 @@
 package team.karakum.showcase.material.inputs
 
+import mui.material.Box
 import mui.material.Button
 import mui.material.ButtonVariant.Companion.contained
 import mui.material.ButtonVariant.Companion.outlined
 import mui.material.ButtonVariant.Companion.text
+import mui.system.sx
 import react.FC
 import react.Props
+import web.cssom.Display
+import web.cssom.JustifyContent
+import web.cssom.em
 
 val ButtonShowcase = FC<Props> {
-    Button {
-        variant = text
-        +"Text"
-    }
-    Button {
-        variant = contained
-        +"Contained"
-    }
-    Button {
-        variant = outlined
-        +"Outlined"
+    Box {
+        sx {
+            display = Display.flex
+            justifyContent = JustifyContent.left
+            gap = 1.em
+        }
+
+        Button {
+            variant = text
+            +"Text"
+        }
+        Button {
+            variant = contained
+            +"Contained"
+        }
+        Button {
+            variant = outlined
+            +"Outlined"
+        }
     }
 }
