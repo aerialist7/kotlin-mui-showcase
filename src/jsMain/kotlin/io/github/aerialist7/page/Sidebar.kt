183720b48b2b53cb5536afd6235fa5123d00a282
Alexander Popoff <alexander.popoff@flysto.net>
2025-12-19 15:37:37 +0200

    Fix import

--- src/jsMain/kotlin/io/github/aerialist7/page/Sidebar.kt
+++ src/jsMain/kotlin/io/github/aerialist7/page/Sidebar.kt
@@ -5,6 +5,7 @@ import io.github.aerialist7.router.useShowcaseIdParam
 import io.github.aerialist7.showcase.Showcase
 import js.uri.decodeURIComponent
 import js.uri.encodeURIComponent
+import js.array.ReadonlyArray
 import mui.material.*
 import mui.system.Box
 import mui.system.sx
