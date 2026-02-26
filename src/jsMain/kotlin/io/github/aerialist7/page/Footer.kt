a88852b8088cc04f23a2da8a1bfded00a18fc20c
Alexander Popoff <alexander.popoff@flysto.net>
2026-02-26 22:09:08 +0000

    Add top shadow to footer bottom navigation panel
    dm-checkpoint-id: 3sZQdc3Wd7Tj

--- src/jsMain/kotlin/io/github/aerialist7/page/Footer.kt
+++ src/jsMain/kotlin/io/github/aerialist7/page/Footer.kt
@@ -17,6 +17,7 @@ val Footer = FC {
     BottomNavigation {
         sx {
             gridArea = Area.Footer
+            boxShadow = "0px -2px 4px -1px rgba(0,0,0,0.2), 0px -4px 5px 0px rgba(0,0,0,0.14), 0px -1px 10px 0px rgba(0,0,0,0.12)".unsafeCast<web.cssom.BoxShadow>()
         }
 
         component = ReactHTML.footer
