e39e3eb6b5030d19ee5d52a1d4cda46a0ca62066
Alexander Popoff <alexander.popoff@flysto.net>
2026-02-27 00:18:20 +0200

    Remove redundand

--- webpack.config.d/dev-server.js
+++
@@ -1,6 +0,0 @@
-// Configure Webpack Dev Server for Matter preview environment
-if (config.devServer) {
-    config.devServer.host = '0.0.0.0';
-    config.devServer.port = 8080;
-    config.devServer.allowedHosts = 'all';
-}
