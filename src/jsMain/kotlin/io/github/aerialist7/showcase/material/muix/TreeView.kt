3848745383a7afe8e0333427af4544f49bfe7c61
Alexander Popoff <alexander.popoff@flysto.net>
2026-02-26 21:44:19 +0000

    Add RichTreeView basic usage example to Tree View showcase
    dm-checkpoint-id: 14MuO00STKUb

--- src/jsMain/kotlin/io/github/aerialist7/showcase/material/muix/TreeView.kt
+++ src/jsMain/kotlin/io/github/aerialist7/showcase/material/muix/TreeView.kt
@@ -1,7 +1,11 @@
 package io.github.aerialist7.showcase.material.muix
 
 import mui.material.Box
+import mui.material.Chip
+import mui.material.Divider
+import mui.material.DividerVariant.Companion.fullWidth
 import mui.system.sx
+import muix.tree.view.RichTreeView
 import muix.tree.view.SimpleTreeView
 import muix.tree.view.TreeItem
 import react.FC
@@ -9,7 +13,53 @@ import react.Props
 import react.ReactNode
 import web.cssom.px
 
+private external interface TreeViewBaseItem {
+    var id: String
+    var label: String
+    var children: Array<TreeViewBaseItem>?
+}
+
+private fun item(id: String, label: String, vararg children: TreeViewBaseItem): TreeViewBaseItem {
+    val obj = js("{}").unsafeCast<TreeViewBaseItem>()
+    obj.id = id
+    obj.label = label
+    if (children.isNotEmpty()) {
+        obj.children = arrayOf(*children)
+    }
+    return obj
+}
+
+private val MUI_X_PRODUCTS = arrayOf(
+    item(
+        "grid", "Data Grid",
+        item("grid-community", "@mui/x-data-grid"),
+        item("grid-pro", "@mui/x-data-grid-pro"),
+        item("grid-premium", "@mui/x-data-grid-premium"),
+    ),
+    item(
+        "pickers", "Date and Time Pickers",
+        item("pickers-community", "@mui/x-date-pickers"),
+        item("pickers-pro", "@mui/x-date-pickers-pro"),
+    ),
+    item(
+        "charts", "Charts",
+        item("charts-community", "@mui/x-charts"),
+    ),
+    item(
+        "tree-view", "Tree View",
+        item("tree-view-community", "@mui/x-tree-view"),
+    ),
+)
+
 val TreeViewShowcase = FC<Props> {
+    Divider {
+        variant = fullWidth
+
+        Chip {
+            label = ReactNode("SimpleTreeView")
+        }
+    }
+
     Box {
         sx {
             minHeight = 352.px
@@ -65,6 +115,25 @@ val TreeViewShowcase = FC<Props> {
                     label = ReactNode("@mui/x-tree-view")
                 }
             }
+        }
+    }
+
+    Divider {
+        variant = fullWidth
+
+        Chip {
+            label = ReactNode("RichTreeView")
+        }
+    }
+
+    Box {
+        sx {
+            minHeight = 352.px
+            minWidth = 250.px
+        }
+
+        RichTreeView {
+            asDynamic().items = MUI_X_PRODUCTS
         }
     }
 }
